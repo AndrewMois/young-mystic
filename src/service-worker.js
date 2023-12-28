f38c54a137f6aac8af6182c7453d50ce08a204f8
Andrew <amoiseyenko@ukr.net>
2023-12-27 20:55:16 -0500

    "Stale While Revalidate" strategy for caching implemented

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,7 +6,7 @@ const CACHE = `cache-${version}`;
 
 const ASSETS = [
 	...build, // the app itself
-	...files,  // everything in `static`
+	...files // everything in `static`
 ];
 
 self.addEventListener('install', (event) => {
@@ -34,47 +34,19 @@ self.addEventListener('fetch', (event) => {
 	// ignore POST requests etc
 	if (event.request.method !== 'GET') return;
 
-	async function respond() {
-		const url = new URL(event.request.url);
-		const cache = await caches.open(CACHE);
+	event.respondWith(
+		(async function () {
+			const cache = await caches.open(CACHE);
+			const cachedResponse = await cache.match(event.request);
 
-		// `build`/`files` can always be served from the cache
-		if (ASSETS.includes(url.pathname)) {
-			const response = await cache.match(url.pathname);
+			const fetchPromise = fetch(event.request)
+				.then((networkResponse) => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				})
+				.catch(() => cachedResponse);
 
-			if (response) {
-				return response;
-			}
-		}
-
-		// for everything else, try the network first, but
-		// fall back to the cache if we're offline
-		try {
-			const response = await fetch(event.request);
-
-			// if we're offline, fetch can return a value that is not a Response
-			// instead of throwing - and we can't pass this non-Response to respondWith
-			if (!(response instanceof Response)) {
-				throw new Error('invalid response from fetch');
-			}
-
-			if (response.status === 200) {
-				cache.put(event.request, response.clone());
-			}
-
-			return response;
-		} catch (err) {
-			const response = await cache.match(event.request);
-
-			if (response) {
-				return response;
-			}
-
-			// if there's no cache, then just error out
-			// as there is nothing we can do to respond to this request
-			throw err;
-		}
-	}
-
-	event.respondWith(respond());
+			return cachedResponse || fetchPromise;
+		})()
+	);
 });
