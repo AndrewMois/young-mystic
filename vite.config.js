1df0fe6f26be0c74bf39a01e5df0c49773e21e8b
Andrew <amoiseyenko@ukr.net>
2023-12-30 18:05:52 -0500

    Change vite conf

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [sveltekit(), SvelteKitPWA({
-		strategies: 'injectManifest',
 		srcDir: 'src',
 		filename: 'sw.js', // or `my-sw.ts`
 		/* other pwa options */
 	})],
-	assetsInclude: ['src/sw.js'],
 });
