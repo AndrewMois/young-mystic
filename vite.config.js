674156bfb7c8cfc77905d74b93bbc30ee3936bf1
Andrew <amoiseyenko@ukr.net>
2023-12-29 22:15:06 -0500

    testing SW

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+
 
 export default defineConfig({
 	plugins: [sveltekit(),
-		VitePWA({ registerType: 'autoUpdate', injectRegister: 'inline' }),
+		SvelteKitPWA({
+			strategies: 'injectManifest',
+			srcDir: 'src',
+			filename: 'service-worker.js',
+		}),
 	],
 });
