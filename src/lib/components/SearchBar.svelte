b0cc7d583971473e6d6ba943d874b3145350e1f6
Andrew <amoiseyenko@ukr.net>
2024-01-02 21:38:05 -0500

    increased search font size

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -58,8 +58,8 @@
 	<label for='search' class='sr-only'>Search</label>
 	<div class='flex rounded-lg shadow-md relative'>
 		<input type='text' id='search' name='search'
-					 class='py-3 px-4 block w-full bg-white border-amber-50 border rounded-xl shadow-sm text-sm focus:border-amber-100 focus:ring-amber-500 disabled:opacity-50 disabled:pointer-events-none placeholder-accent'
-					 class:rounded-b-none={suggestions && suggestions.length > 0}
+					 class='py-3 px-4 block w-full bg-white border-amber-50 border text-lg rounded-xl shadow-sm focus:ring-0 disabled:opacity-50 disabled:pointer-events-none placeholder-accent'
+					 class:rounded-b-none={!hideSuggestions && suggestions && suggestions.length > 0}
 					 placeholder='Поиск'
 					 bind:value={search} on:input={updateSuggestions} on:keydown={handleKeydown} on:blur={handleBlur}
 					 on:focus={updateSuggestions}
@@ -67,7 +67,7 @@
 	</div>
 
 	{#if !hideSuggestions && suggestions && suggestions.length > 0}
-		<ul class='absolute bg-white border-b border-y border-gray-200 rounded-b-md shadow-lg w-full'>
+		<ul class='absolute bg-white border-b border-y border-gray-200 rounded-b-md shadow-lg w-full text-lg'>
 			{#each suggestions as suggestion, index (suggestion.slug.current)}
 				{#if suggestion.slug.current === 'none'}
 					<li class='block px-4 py-2'>{suggestion.nameEn}</li>
@@ -78,7 +78,7 @@
 							 class:background-primary={index === selectedIndex}>
 							{suggestion.nameEn}
 							{#if suggestion.nameRu}
-								<span class='text-gray-500 font-bold text-xs ml-0.5'>{suggestion.nameRu}</span>
+								<span class='text-gray-500 font-bold text-sm ml-0.5'>{suggestion.nameRu}</span>
 							{/if}
 						</a>
 					</li>
