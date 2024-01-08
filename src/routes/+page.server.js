cdea85b92b4c580bf027b1f2c7d7594e62fb8541
Andrew <amoiseyenko@ukr.net>
2024-01-07 23:07:41 -0500

    added main menu to Sanity connection

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,24 @@
 import { redirect } from '@sveltejs/kit';
+import { client } from '../sanityClient.js';
 
 export async function load({ locals }) {
 	if (!locals.authedUser) {
 		throw redirect(302, '/login');
 	}
+
+	const lang = locals.lang ? locals.lang : 'ru';
+	const data = await client.fetch(`*[_type == "menu" && language == "${lang}" && visible == true]
+	{title, slug, description, active, 
+	"image": image.asset->url}`,
+	);
+
+	if (data && data.length > 0) {
+		return {
+			menu: data,
+		};
+	}
+	return {
+		status: 500,
+		body: new Error('Couldn\'t load menu'),
+	};
 }
