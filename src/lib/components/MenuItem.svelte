9e8da288ea2b8b9e4b955ee8747ea5c5f6f75ca5
Andrew <amoiseyenko@ukr.net>
2024-01-16 19:22:19 -0500

    Profile button adjusted

--- src/lib/components/MenuItem.svelte
+++ src/lib/components/MenuItem.svelte
@@ -4,12 +4,14 @@
 </script>
 
 <a href='/profile'
-	 class='flex items-center h-16 w-full rounded-xl bg-white shadow-md md:flex-row'>
-	<div class='flex items-center background-faded h-full rounded-l-xl'>
-		<img src={icon} alt={title + ' icon'}
-				 class='mx-4 h-10'>
-	</div>
-	<h5 class='text-2xl tracking-tight ml-4'>
+	 class='flex items-center justify-between py-3 px-4 w-full rounded-xl bg-white shadow-md md:flex-row'>
+	<!--	<div class='flex items-center background-faded h-full rounded-l-xl'>-->
+	<h5 class='text-xl tracking-tight'>
 		{title}
 	</h5>
+	
+	<img src={icon} alt={title + ' icon'}
+			 class='h-8'>
+	<!--	</div>-->
+
 </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 	import Wrapper from '$components/Wrapper.svelte';
-	import SummaryPanel from '$components/SummaryPanel.svelte';
 	import SearchBar from '$components/SearchBar.svelte';
 	import Card from '$components/Card.svelte';
 	import TopBackground from '$components/TopBackground.svelte';
 	import locales from '$lib/locales/menu/locales.json';
 	import Profile from '$lib/icons/Profile.svg';
 	import MenuItem from '$components/MenuItem.svelte';
+	import MenuPanel from '$components/MenuPanel.svelte';
 
 	export const load = async ({ parent }) => {
 		await parent();
@@ -25,7 +25,7 @@
 
 <TopBackground />
 <Wrapper mobilePadding={true} extraClasses='-mt-20'>
-	<SummaryPanel>
+	<MenuPanel>
 		<div class='flex flex-col items-center justify-center gap-4'>
 
 			<SearchBar {locales} {lang} />
@@ -35,10 +35,10 @@
 							active={item.active}
 							image={item.image ? item.image : undefined} />
 			{/each}
-			
+
 			<MenuItem title={locales.profile[lang]} icon={Profile} />
 
 		</div>
-	</SummaryPanel>
+	</MenuPanel>
 </Wrapper>
 
