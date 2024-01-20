a9413b5224344c845595ac597b63adc93bf0d8af
Andrew <amoiseyenko@ukr.net>
2024-01-19 19:37:11 -0500

    small sketch of oil page

--- src/lib/components/SummaryPanel.svelte
+++ src/lib/components/SummaryPanel.svelte
@@ -1,3 +1,8 @@
-<section class='w-full rounded-t-3xl bg-amber-50 border border-accent background-accent px-3 pt-4 pb-6'>
+<script>
+	export let extraClasses = '';
+</script>
+
+<section
+	class={`w-full rounded-t-3xl bg-amber-50 border border-accent background-accent px-3 pt-4 pb-6 ${extraClasses ? extraClasses : ''}`}>
 	<slot />
 </section>
--- src/routes/oils/[slug]/+page.svelte
+++ src/routes/oils/[slug]/+page.svelte
@@ -1,15 +1,28 @@
 <script>
 
+	import Wrapper from '$components/Wrapper.svelte';
+	import TopBackground from '$components/TopBackground.svelte';
+	import SummaryPanel from '$components/SummaryPanel.svelte';
+
 	export let data;
 	$: oilData = data.oil;
 </script>
 <svelte:head>
-	<title>{oilData.nameRu} | YM</title>
+	<title>{oilData.nameEn} | YM</title>
 	<meta name='description' content={`Узнайте всё об эфирном масле ${oilData.nameRu} в одном месте.`} />
 </svelte:head>
 
-<h1>Oil page</h1>
+<TopBackground />
+<Wrapper mobilePadding={true} extraClasses='-mt-20'>
+	<SummaryPanel extraClasses='mb-4'>
+		<div class='flex flex-col items-center justify-center gap-4'>
 
-<h2>{oilData.nameEn}</h2>
-<p>{oilData.description}</p>
+			<h2>{oilData.nameEn}</h2>
+			<p>{oilData.description}</p>
+
+		</div>
+	</SummaryPanel>
+</Wrapper>
+
+
 
