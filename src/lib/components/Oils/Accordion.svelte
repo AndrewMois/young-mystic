1c8697b02cafdeb692b539cbec5eb9d60d035400
Andrew <amoiseyenko@ukr.net>
2024-01-23 22:36:29 -0500

    Added bulletlist and adjusted margins

--- src/lib/components/Oils/Accordion.svelte
+++ src/lib/components/Oils/Accordion.svelte
@@ -6,27 +6,41 @@
 </script>
 
 {#each sections as section (section._key)}
-	<div class='w-full mb-3 border-b border-faded'>
-		<button class='w-full flex items-center gap-3 py-3 text-xl'
-						on:click={() => section.open = !section.open}>
-			<svg class={`transition-transform duration-500 ease-in-out ${section.open ? 'rotate-180' : ''}`}
-					 xmlns='http://www.w3.org/2000/svg' width='24' height='24'
-					 viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round'
-					 stroke-linejoin='round'>
-				<path d='m6 9 6 6 6-6' />
+	<div class="w-full mb-3 border-b border-faded">
+		<button
+			class="w-full flex items-center gap-3 py-3 text-xl"
+			on:click={() => (section.open = !section.open)}
+		>
+			<svg
+				class={`transition-transform duration-500 ease-in-out ${section.open ? 'rotate-180' : ''}`}
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path d="m6 9 6 6 6-6" />
 			</svg>
-			{section.name}
+			<h5 class="text-left">{section.name}</h5>
 		</button>
 		{#if section.open}
-			<div class='flex flex-col gap-2 mb-2'>
+			<div class="flex flex-col gap-2 mb-2 ml-3">
 				{#each section.content as block (block._key)}
 					{#each block.children as child (child._key)}
 						{#if block.style === 'h3'}
-							<h3 class='font-bold'>{child.text}</h3>
-						{:else if block.style === 'normal'}
-							<p>{child.text}</p>
+							<h3 class="font-bold">{child.text}</h3>
+						{:else if block.style === 'normal' && block.listItem !== 'bullet'}
+							<p class="leading-relaxed">{child.text}</p>
 						{:else if block.style === 'blockquote'}
-							<blockquote class='border-l-2 border-accent pl-3 italic'>{child.text}</blockquote>
+							<blockquote class="border-l-2 border-accent pl-3 mt-2 italic">
+								{child.text}
+							</blockquote>
+						{:else if block.listItem === 'bullet'}
+							<p class="leading-relaxed"><span class="font-bold">&#8226</span> {child.text}</p>
 						{/if}
 					{/each}
 				{/each}
@@ -34,4 +48,3 @@
 		{/if}
 	</div>
 {/each}
-
