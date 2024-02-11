cf03dbe853714ea7af082e164727e8f344802d70
Andrew <amoiseyenko@ukr.net>
2024-02-11 13:09:49 -0500

    prettified BlendIngredients.svelte

--- src/lib/components/Oils/BlendIngredients.svelte
+++ src/lib/components/Oils/BlendIngredients.svelte
@@ -6,14 +6,18 @@
 {#if title && ingredients.length > 0}
 	<div class='w-full flex flex-col gap-1'>
 		<p class='font-semibold px-3 '>{title}:</p>
-		{#each ingredients as ingredient}
-			<a href={ingredient.slug}
-				 class='px-3 py-2 background-primary border border-accent rounded-xl'>
-				<p class='font-semibold'>{ingredient.nameEn}</p>
-				{#if ingredient.nameRu}
-					<p class='text-xs'>{ingredient.nameRu}</p>
-				{/if}
-			</a>
-		{/each}
+		<div class='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 '>
+			{#each ingredients as ingredient}
+				<a href={ingredient.slug}
+					 class='py-2 px-3 background-primary border border-accent rounded-xl flex items-center'>
+
+					{#if ingredient.nameRu}
+						<p class=''>{ingredient.nameRu}</p>
+					{:else if ingredient.nameEn}
+						<p class=''>{ingredient.nameEn}</p>
+					{/if}
+				</a>
+			{/each}
+		</div>
 	</div>
 {/if}
