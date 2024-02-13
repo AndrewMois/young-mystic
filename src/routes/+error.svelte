d8fe35de881672091c76b5228d83e2598a20ded6
Andrew <amoiseyenko@ukr.net>
2024-02-12 23:01:51 -0500

    error 404 page added for main pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,7 @@
 	import lottie from 'lottie-web';
 	import { onMount } from 'svelte';
 	import locales from '$lib/locales/error/locales.json';
+	import Footer from '$components/Footer.svelte';
 
 	export let data;
 	const lang = data.lang;
@@ -25,7 +26,7 @@
 <div class='flex flex-col items-center justify-center text-center'>
 	<div id='gif'></div>
 	<h1 class='text-4xl font-bold text-accent font-cormorant'>{locales.oops[lang]}</h1>
-	<p class='mt-4 text-lg'>{locales.oilNotFoundText[lang]}</p>
+	<p class='mt-4 text-lg'>{locales.generalNotFoundText[lang]}</p>
 	<p class='mt-2 text-sm'>{locales.contactUs[lang]}</p>
 	<a href='/oils'
 		 class='mt-6 px-4 py-2 font-semibold background-accent rounded'>{locales.backToLibrary[lang]}</a>
@@ -33,5 +34,6 @@
 	{#if $page.error.code}
 		<p class='text-xs mt-1 mb-4'>{locales.refCode[lang]}: {$page.error.code}</p>
 	{/if}
-
 </div>
+<Footer />
+
