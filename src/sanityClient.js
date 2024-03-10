c0d56f0d1915fb20d522e8859080a73f1a8d80cc
Andrew <amoiseyenko@ukr.net>
2024-03-10 17:39:06 -0400

    Sanity client updated

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client';
 export const client = createClient({
 	projectId: 'lttjxemu',
 	dataset: 'production',
-	apiVersion: '2023-11-25',
+	apiVersion: '2024-03-10',
 	useCdn: false,
 });
