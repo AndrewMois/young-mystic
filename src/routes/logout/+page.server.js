8f9745a13608732c4fe50055f58a535039f80166
Andrew <amoiseyenko@ukr.net>
2024-03-10 19:17:57 -0400

    hotfix for Logout issue

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -1,7 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load({ cookies, locals }) {
-	cookies.set('authToken', '', { maxAge: 0 }, { path: '/' });
+	cookies.set('authToken', '', { maxAge: 0, path: '/' });
 	locals.authedUser = undefined;
 
 	redirect(302, '/login');
