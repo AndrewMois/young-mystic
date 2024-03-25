58c67db6c91830aaab511ea0124a8a027f102d9c
Andrew <amoiseyenko@ukr.net>
2024-03-24 20:40:56 -0400

    main page pop-up was added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 	import SupportIcon from '$lib/icons/Support.svg';
 	import MenuItem from '$components/MenuItem.svelte';
 	import MenuPanel from '$components/MenuPanel.svelte';
+	import DemoPopUp from '$components/DemoPopUp.svelte';
 
 	export const load = async ({ parent }) => {
 		await parent();
@@ -42,3 +43,21 @@
 	</MenuPanel>
 </Wrapper>
 
+<DemoPopUp title='Idea of the app'
+					 description="
+					 <p>As a community leader and essential oils enthusiast, I wanted to create a <span class='border-b-2 border-accent'>handy essential oils library</span> with a focus on <span class='border-b-2 border-accent'>simplicity and ease of use</span> for members of all ages.</p>
+      <p>We were relying on a lot of thick books and encyclopaedias that were difficult to keep handy.</p>
+      <p class='font-bold'>What has been done:</h3>
+      <ul class='list-disc ml-4'>
+        <li>Designed and created from scratch</li>
+        <li>App is available for installation on all platforms as a <span class='border-b-2 border-accent'>PWA.</span></li>
+        <li>A library of over 200 oils in Russian (Ukrainian and English are being translated).</li>
+        <li>App is available in <span class='border-b-2 border-accent'>three languages</span> and more can be easily added in the future.</li>
+      </ul>
+      <p class='font-bold'>Features under development:</h3>
+      <ul class='list-disc ml-4'>
+        <li>Community news section</li>
+        <li>Online courses section with the possibility to buy a course</li>
+        <li>Paid subscriptions for non-community members</li>
+      </ul>"
+					 button='Got it!' />
